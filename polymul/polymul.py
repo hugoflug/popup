de68ac69f7aa1606b24a2cda29f475b0b05f3a8e
Hugo Sandelius <hugo.sandelius@gmail.com>
2015-09-20 17:37:15 +0200

    polymul: added beginnings of python implementation of karatsuba's algorithm

---
+++ polymul/polymul.py
@@ -0,0 +1,34 @@
+"""
+procedure karatsuba(num1, num2)
+  if (num1 < 10) or (num2 < 10)
+    return num1*num2
+  /* calculates the size of the numbers */
+  m = max(size_base10(num1), size_base10(num2))
+  m2 = m/2
+  /* split the digit sequences about the middle */
+  high1, low1 = split_at(num1, m2)
+  high2, low2 = split_at(num2, m2)
+  /* 3 calls made to numbers approximately half the size */
+  z0 = karatsuba(low1,low2)
+  z1 = karatsuba((low1+high1),(low2+high2))
+  z2 = karatsuba(high1,high2)
+  return (z2*10^(2*m2))+((z1-z2-z0)*10^(m2))+(z0)
+"""
+
+def quadmul(pol1, pol2):
+	pass
+
+def karatsuba(pol1, pol2):
+	if len(pol1) < 10 or len(pol2) < 10:
+		return quadmul(pol1, pol2)
+	m = max(len(pol1), len(pol2))
+	m2 = m // 2
+	low1 = pol1[:m2]
+	high1 = pol1[m2:]
+	low2 = pol2[:m2]
+	high2 = pol2[m2:]
+
+	z0 = karatsuba(low1, low2)
+	z1 = karatsuba((low1+high1), (low2+high2))
+	z2 = karatsuba(high1, high2)
+	return (z2*10**(2*m2)) + ((z1-z2-z0)*10**(m2))+(z0)
