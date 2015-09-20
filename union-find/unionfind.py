b77517a6374decd46c00a506bbfcdbcb4a7fc5e2
Hugo Sandelius <hugo.sandelius@gmail.com>
2015-09-20 13:53:02 +0200

    unionfind: fixed case where x and y are in the same set in union(x,y) and changed to py2

--- union-find/unionfind.py
+++ union-find/unionfind.py
@@ -36,6 +36,9 @@ def union(x, y):
     x_rep = find(x)
     y_rep = find(y)
 
+    if x_rep == y_rep:
+        return
+
     if x_rep.height > y_rep.height:
         y_rep.parent = x_rep
     elif y_rep.height > x_rep.height:
@@ -50,7 +53,7 @@ for _ in range(Q):
     op, set1, set2 = sys.stdin.readline().strip("\n").split(" ")
     
     if op == "?":
-        print("yes" if same(set1, set2) else "no")
+        print "yes" if same(set1, set2) else "no"
         
     if op == "=":
         union(set1, set2)
