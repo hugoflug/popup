820104ada8650a84d01c8d4d3ff892d8db227551
Fabian Maximilian Schilling <fabian.schilling@me.com>
2015-09-23 21:59:09 +0200

    Re-implemented IntervalCover in Java

---
+++ intervalcover/src/Interval.java
@@ -0,0 +1,12 @@
+public class Interval {
+
+    public double a; // I know this is bad style!
+    public double b;
+    public int index;
+
+    public Interval(double a, double b, int index) {
+        this.a = a;
+        this.b = b;
+        this.index = index;
+    }
+}
